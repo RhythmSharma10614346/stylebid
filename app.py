8650862179453cdadccb51110d335bcfdc644d95
RhythmSharma10614346 <10614346@mydbs.ie>
2023-04-05 19:50:41 +0100

    fixing errors

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__ , static_folder:= "/static")
 
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/auction')
+def auction():
+	return render_template("auction.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
